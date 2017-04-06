2f2d5afc5e2915c43150b916bee0ba339879bb8e
udon <naoki_udon@outlook.com>
2017-04-07 00:11:33 +0900

    CharacterModel,CharacterManager,TresureModelを定義、各種少しずつパラメーターを設定

---
+++ TGS/Assets/Scripts/Character/CharacterManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonCommons;
+
+public class CharacterManager : UdonBehaviourSingleton<CharacterManager> {
+
+    private List<CharacterModel> _characterModels;
+    public List<CharacterModel> CharacterModels
+    {
+        get
+        {
+            if(_characterModels == null)
+            {
+                InstantLog.StringLogError("characterModels is null");
+                _characterModels = new List<CharacterModel>();
+            }
+
+            return _characterModels;
+        }
+    }
+
+    public CharacterModel GetCharacterModel(int index)
+    {
+        if(_characterModels == null)
+        {
+            InstantLog.StringLogError("characterModels is null");
+            return null;
+        }
+        else
+        {
+            if(_characterModels.Count <= index || _characterModels.ElementAt(index) == null)
+            {
+                InstantLog.StringLogError("access to characterModels is wrong");
+                return null;
+            }
+            else
+            {
+                return _characterModels.ElementAt(index);
+            }
+        }
+    }
+
+}
---
+++ TGS/Assets/Scripts/Character/CharacterModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UdonCommons;
+
+public class CharacterModel : UdonBehaviour {
+
+    public CharacterModel()
+    {
+        _score = new ReactiveProperty<int>(0);
+        _tresures = new ReactiveCollection<TresureModel>();
+    }
+
+    private ReactiveProperty<int> _score;
+    public IReadOnlyReactiveProperty<int> Score
+    {
+        get
+        {
+            if(_score == null)
+            {
+                InstantLog.StringLogError("score property is null");
+                _score = new ReactiveProperty<int>(0);
+            }
+            return _score;
+        }
+    }
+
+    private ReactiveCollection<TresureModel> _tresures;
+    public IReadOnlyReactiveCollection<TresureModel> Tresures
+    {
+        get
+        {
+            if(_tresures == null)
+            {
+                InstantLog.StringLogError("score property is null");
+                _tresures = new ReactiveCollection<TresureModel>();
+            }
+
+            return _tresures;
+        }
+    }
+
+    public void SetScore(int score)
+    {
+        _score.Value = score;
+    }
+
+    public void AddScore(int score)
+    {
+        _score.Value += score;
+    }
+}
