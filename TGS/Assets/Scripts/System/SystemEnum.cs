dced231672e2faa62d4c7d6be4d2edf118b6b25d
udon <naoki_udon@outlook.com>
2017-04-08 22:10:52 +0900

    評価演算を仮実装

--- TGS/Assets/Scripts/System/SystemEnum.cs
+++ TGS/Assets/Scripts/System/SystemEnum.cs
@@ -6,13 +6,23 @@ namespace SystemParameter
 {
     public static class GameEnum
     {
-        public enum tresureColor {nothing, red, blue, yellow, green }
+        public enum tresureColor { nothing, red, blue, yellow, green }
 
-        public enum direction { left, up, down, right}
+        public enum direction { left, up, down, right }
     }
 
     public static class GameValue
     {
         public static readonly float MOVE_BASE_SPEED = 0.01f;
+
+        public static readonly int SCORE_BASE = 100;
+
+        public static readonly int SCORE_RATE_NUMBER = 100;
+
+        public static readonly int SCORE_RATE_COLOR = 100;
+
+        public static readonly int SCORE_RATE_PLAYER = 300;
+
+        public static readonly float SCORE_RATE_CALCULATE = 1.2f;
     }
 }
--- TGS/Assets/Scripts/Tresure/TresureJudge.cs
+++ TGS/Assets/Scripts/Tresure/TresureJudge.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using System.Linq;
 using SystemParameter;
 using UdonCommons;
@@ -7,9 +8,26 @@ using UnityEngine;
 
 public class TresureJudge{
 
-    private int CalculateScore()
+    private int CalculateScore(List<ColorModel> models)
     {
+        if(models == null)
+        {
+            InstantLog.StringLogError("Calculate models is null");
+            return 0;
+        }
 
+        var score = 0;
+        var array = models.ToArray();
+        var colorCount = array.Select(x => x.TresureColor).Distinct().Count();
+        var modelCount = array.Length;
+        var hasPlayer = array.Where(x => x is CharacterModel).Any();
+
+        score =
+            (int)Mathf.Pow(GameValue.SCORE_RATE_CALCULATE, colorCount) * GameValue.SCORE_RATE_COLOR +
+            (int)Mathf.Pow(GameValue.SCORE_RATE_CALCULATE, modelCount) * GameValue.SCORE_RATE_NUMBER +
+            Convert.ToInt32(hasPlayer) * GameValue.SCORE_RATE_PLAYER;
+
+        return score;
     }
 
 	public bool JudgeTresures(GameEnum.tresureColor player, List<TresureModel> tresures)
