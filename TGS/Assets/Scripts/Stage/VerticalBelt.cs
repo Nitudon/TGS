c9056279cae380321a85c041b58d53316c93bd05
udon <naoki_udon@outlook.com>
2017-06-08 03:47:42 +0900

    ベルトコンベアを実装、床や壁を調整

---
+++ TGS/Assets/Scripts/Stage/VerticalBelt.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonCommons;
+
+public class VerticalBelt : MonoBehaviour {
+
+    private static readonly Vector3 SPIN_FORCE = new Vector3(0f,0f,14.2f);
+
+    [SerializeField]
+    private Renderer BeltRenderer;
+
+    [SerializeField]
+    private float scrollSpeed = 1.5f;
+
+    [SerializeField]
+    private bool reflect = false;
+
+    void FixedUpdate()
+    {
+        float offset = reflect ? Time.time * scrollSpeed : Time.time * -scrollSpeed;
+        BeltRenderer.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+
+    private void OnCollisionStay(Collision col)
+    {
+        if (ExtensionGameObject.HasComponent<CharacterModel>(col.gameObject))
+        {
+            AddBeltForceToRigitbody(col.gameObject);
+        }
+    }
+
+    private void AddBeltForceToRigitbody(GameObject chara)
+    {
+        var force = reflect ? -SPIN_FORCE : SPIN_FORCE;
+        var rigit = chara.GetComponent<Rigidbody>();
+
+        rigit.AddForce(force);
+    }
+}
